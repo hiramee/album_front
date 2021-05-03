e477e43623601ab8f94ad4f5d6b675ccbb82b8bf
hiramee <katsugirogiro951227@gmail.com>
2021-05-04 00:28:16 +0900

    Add Home Page

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # album_front
+自作アルバムアプリのフロントエンド実装用リポジトリです
+サーバ実装は[こちら](https://github.com/hiramee/album_server)
 
-## Demo
+## デモ
 https://album.hiramengo.com/
+
+## 主要フレームワーク/ライブラリ
+- Vue.js 2.X  
+jsフレームワーク.SPA作成に向いていて記述が簡素なので採用した.
+- Vuex  
+状態管理用ライブラリ.タグ情報やログイン情報を共通管理するために使用した.
+- vue-property-decorator  
+デコレータライブラリ.Vueファイルの可読性向上のために使用した.
+- Vuetify 2.X  
+UIフレームワーク.使用経験のあるElementUI以外でメジャーなものを採用した.
+- VeeValidate  
+バリデーション用ライブラリ.Vuetifyの入力コンポーネントでサポートされているので採用した.
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,101 @@
 <template>
   <div class="home">
     <Header />
+    <section class="mx-10">
+      <h1>Album App</h1>
+      <p class="subtitle-1">
+        画像にタグを付けてクラウドに保存するアプリケーションです。<br />画像はアカウント別に保管されているので安心です。
+      </p>
+      <h2>使い方</h2>
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header>利用登録 </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <ol>
+              <li>Sign inボタンからメールアドレスとパスワードを入力します。</li>
+              <li>認証コードがメール送信されるので続けて入力してください。</li>
+            </ol>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header>ログイン </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <ol>
+              <li>
+                Sign inボタンからメールアドレスとパスワードを入力してください。
+              </li>
+            </ol>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header>画像登録 </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <ol>
+              <li>雲マークをクリックします。</li>
+              <li>ダイアログが開くのでファイルを選択します。</li>
+              <li>
+                タグを選択します。新しいタグを追加した場合は直接入力してエンタ―キーを押してください。
+              </li>
+            </ol>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header>画像検索 </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <ol>
+              <li>ナビゲーションバーの入力欄でタグを選択してください。</li>
+              <li>虫眼鏡マークを押してください。</li>
+            </ol>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header>タグの削除</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <ol>
+              <li>右上の三つ点ボタンを押し、Delete tagsを選択してください。</li>
+              <li>
+                ダイアログが開くので削除するタグを選択してください。使用している画像があるタグは削除できません。
+              </li>
+            </ol>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header
+            >ログアウト/パスワード変更</v-expansion-panel-header
+          >
+          <v-expansion-panel-content>
+            <ol>
+              <li>右上の三つ点ボタンを押し、メニューから選択してください。</li>
+            </ol>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </section>
+    <v-footer padless absolute>
+      <v-card width="100%" class="grey lighten-4 text-center py-3">
+        <div>
+          <v-icon>mdi-github</v-icon
+          ><a href="https://github.com/hiramee/album_server" class="mx-2"
+            >Server</a
+          >
+          <a href="https://github.com/hiramee/album_front">Client</a>
+        </div>
+        <div>
+          <v-icon>mdi-email-outline</v-icon>
+          <span class="mx-2">hrm951227@gmail.com</span>
+        </div>
+      </v-card>
+    </v-footer>
   </div>
 </template>
 
@@ -15,3 +110,8 @@ import Header from "../components/Header.vue";
 })
 export default class Home extends Vue {}
 </script>
+<style scoped>
+section {
+  text-align: left;
+}
+</style>
